a738f11b5c73fde745722d222d1a3112cab72d84
FlozyTess <cecilrency23@gmail.com>
2024-12-05 03:30:17 +0300

    completed lab

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ var customerName = 'bob';
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
 }
+// Function to declare a variable in global scope and assign a value
+function setBestCustomer() {
+    bestCustomer = 'not bob'; }
+// Function to overwrite the value of the global bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'john';
+
+// Function to try changing the constant value
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'peter'; 
+}
